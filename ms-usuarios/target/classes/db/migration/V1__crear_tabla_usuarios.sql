015008f332a4f20aad08451d5a3c6f6c61ee1a17
Fran-informatica <fran.figueroad@duocuc.cl>
2026-05-15 21:50:37 -0400

    delete target/gitignore/gitattributes obsolete

--- ms-usuarios/target/classes/db/migration/V1__crear_tabla_usuarios.sql
+++
@@ -1,9 +0,0 @@
--- Creación de tabla usuarios
-CREATE TABLE usuarios (
-    id_usuario   INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre       TEXT NOT NULL,
-    email        TEXT NOT NULL UNIQUE,
-    password     TEXT NOT NULL,
-    rol          TEXT NOT NULL,
-    activo       INTEGER NOT NULL DEFAULT 1
-);
